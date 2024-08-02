2a276a3449029e7cc2aec59f91d257f248d390bd
Pouya TJ <tajdary.pouya@gmail.com>
2024-08-02 18:27:20 +0200

    creepFitting: changed with seperate functions and main function - mainProg: tkinter programm to run creep fitting and see the result

--- creepFitting.py
+++ creepFitting.py
@@ -6,113 +6,129 @@ from scipy.optimize import curve_fit
 from lmfit import Model
 
 
-# Replace 'your_file.xlsx' with the actual file name
+
+
+
+def creepFit(df,sigmaList,tempList,numberList,option):
+    
+    if type(sigmaList) is str:
+        sigmaList=[int(x) for x in sigmaList.split(',')]
+        tempList=[int(x) for x in tempList.split(',')]
+        numberList=[int(x) for x in numberList.split(',')]       
+
+    listOdd=list(range(1,df.shape[1],2))
+    listEven=list(range(0,df.shape[1],2))
+    colors=['blue','green','red','orange','yellow','purple','black','cyan','grey','brown']
+    # plt.figure(figsize=(8, 6))
+    fig,ax = plt.subplots()
+    i=0
+    for j in range(0,int(len(tempList))):
+        for k in range(0,numberList[j]):
+            
+            #converting the E-modulus(MPa) v.s. time (h) to strain v.s. time (s)
+            df[listOdd[i]]=df[listOdd[i]].map(lambda x: sigmaList[i]/x)
+            df[listEven[i]]=df[listEven[i]].map(lambda x: x*3600)
+            #Plotting the experimental data
+            ax.plot(df[listEven[i]].dropna(),df[listOdd[i]].dropna(), label=f'{tempList[j]}°C-{sigmaList[i]}MPa Data',color=colors[i])
+            i+=1
+
+
+
+
+    ############## Calibration
+
+    # Combine the data
+    combined_time = np.concatenate([df[listEven[i]].dropna() for i in range(0,len(listEven))])
+    combined_epsilon = np.concatenate([df[listOdd[i]].dropna() for i in range(0,len(listEven))])
+
+    # Combine the corresponding stress values
+    combinedSigma = np.concatenate([np.full_like(df[listOdd[i]].dropna(),sigmaList[i]) for i in range(0,int(len(sigmaList)))])
+
+    # Temperature
+    combinedTemp_temp=[tempList[i] for i in range(len(numberList)) for j in range(numberList[i]) ]
+    combinedTemp = np.concatenate([np.full_like(df[listOdd[i]].dropna(),combinedTemp_temp[i]) for i in range(0,int(len(sigmaList)))])+273
+
+
+    # Creep strain model function
+    def creep_model(t,  C1, C2, C3, C4):
+        return ((1 / (combinedSigma**C2 * np.exp(-C4 / combinedTemp) * C1 * t * (1 - C3)))**(1 / (C3 - 1)))
+
+
+    if option=='LMFIT':
+  
+
+
+        ### LMFIT MODEL ###
+        model = Model(creep_model)
+        parameters = model.make_params(C1=0.1,C2=20,C3=-20,C4=1000)
+        parameters['C1'].set(min=0.00000000001,max=10)
+        parameters['C2'].set(min=0.,max=50)
+        parameters['C3'].set(min=-50,max=0)
+        parameters['C4'].set(min=1,max=100000)
+        result = model.fit(combined_epsilon,parameters,t=combined_time)
+
+        # Print the optimized parameters
+        print("Optimized parameters for Sigma = 5 MPa:", result.fit_report())
+
+        params = []
+        # Extract all parameters into a dictionary
+        params_dict = {param_name: (param.value, param.stderr) for param_name, param in result.params.items()}
+
+        # Extract the parameters
+        for param_name, (value, stderr) in params_dict.items():
+            params.append(value)
+    else:
+        ### CURVE FIT MODEL ###
+        # Initial guess for parameters C1, C2, C3, C4
+        initial_guess = ([0.0000000001, 0, -50,0], [1, 50, 0,np.inf])
+        # Fit the model to the experimental data for Sigma 
+        params, covariance = curve_fit(creep_model, combined_time, combined_epsilon, bounds=initial_guess,maxfev=100000)
+        # Print the optimized parameters
+        print("Optimized parameters for Sigma = 5 MPa:", params)
+
+
+    # Plot the experimental data and the fitted model
+    def creep_model_test(t,stress,temp,C1,C2,C3,C4):
+        return ((1 / (stress**C2 * np.exp(-C4 / (273+temp)) * C1 * t * (1 - C3)))**(1 / (C3 - 1)))
+
+    i=0
+    for sigma in sigmaList:
+        timeSpace = np.logspace(np.log10(df[listEven[i]][0]), np.log10(df[listEven[i]][len(df[listEven[i]].dropna())-1]), num=100)
+        ax.plot(timeSpace,creep_model_test(timeSpace,sigma,combinedTemp_temp[i],*params),label=f'Fitted Model - {combinedTemp_temp[i]}°C - {sigma} MPa',linestyle='--',color=colors[i])
+        i=i+1
+            
+
+    #Figure modification
+    ax.set_xlabel('Time')
+    # plt.xscale('log')
+    ax.set_ylabel('Creep Strain')
+    ax.set_title(f'Creep Strain Model Calibration for parameter: \n [C1 C2 C3 C4] = {params}')
+    ax.legend()
+    ax.grid(True)
+    
+    return params,fig
+
+
+    # Replace 'your_file.xlsx' with the actual file name
 file_path = '3G fitting data _2 temp.xlsx'
 
 
-# Load the data from Excel
-df = pd.read_excel(file_path,2,header=None)
+def main():
+    # Load the data from Excel
+    df = pd.read_excel(file_path,header=None)
 
-# Extract time and creep strain columns for both temperatures
-listOdd=list(range(1,df.shape[1],2))
-listEven=list(range(0,df.shape[1],2))
+    # Extract time and creep strain columns for both temperatures
+    sigmaList=[5,10,15,20,5,10]
+    tempList=[23,40]
+    numberList=[4,2]
+    option=None
 
-sigmaList=[5,10,15,20,5,10]
-tempList=[23,40]
-numberList=[4,2]
-colors=['blue','green','red','orange','yellow','purple','black','cyan','grey','brown']
-plt.figure(figsize=(8, 6))
-i=0
-for j in range(0,int(len(tempList))):
-    for k in range(0,numberList[j]):
-        
-        #converting the E-modulus(MPa) v.s. time (h) to strain v.s. time (s)
-        df[listOdd[i]]=df[listOdd[i]].map(lambda x: sigmaList[i]/x)
-        df[listEven[i]]=df[listEven[i]].map(lambda x: x*3600)
-        #Plotting the experimental data
-        plt.plot(df[listEven[i]].dropna(),df[listOdd[i]].dropna(), label=f'{tempList[j]}°C-{sigmaList[i]}MPa Data',color=colors[i])
-        i+=1
+    parameters, plot = creepFit(df,sigmaList,tempList,numberList,option)
 
+    # Show the plot
+    plot.savefig('fitted_data_40C lmfit', dpi=300)
+    plt.show()
 
-
-
-############## Calibration
-
-# Combine the data
-combined_time = np.concatenate([df[listEven[i]].dropna() for i in range(0,len(listEven))])
-combined_epsilon = np.concatenate([df[listOdd[i]].dropna() for i in range(0,len(listEven))])
-
-# Combine the corresponding stress values
-combinedSigma = np.concatenate([np.full_like(df[listOdd[i]].dropna(),sigmaList[i]) for i in range(0,int(len(sigmaList)))])
-
-# Temperature
-combinedTemp_temp=[tempList[i] for i in range(len(numberList)) for j in range(numberList[i]) ]
-combinedTemp = np.concatenate([np.full_like(df[listOdd[i]].dropna(),combinedTemp_temp[i]) for i in range(0,int(len(sigmaList)))])+273
-
-# T = 318  # Replace with your actual temperature
-#C4=13000
-# Creep strain model function
-# def creep_model(t, C1, C2, C3, C4):
-#     return ((1 / (combinedSigma**C2 * np.exp(-C4 / combined_temp) * C1 * t * (1 - C3)))**(1 / (C3 - 1)))
-def creep_model(t,  C1, C2, C3, C4):
-    return ((1 / (combinedSigma**C2 * np.exp(-C4 / combinedTemp) * C1 * t * (1 - C3)))**(1 / (C3 - 1)))
-
-
-
-### CURVE FIT MODEL ###
-# Initial guess for parameters C1, C2, C3, C4
-initial_guess = ([0.0000000001, 0, -50,0], [1, 50, 0,np.inf])
-# Fit the model to the experimental data for Sigma 
-params, covariance = curve_fit(creep_model, combined_time, combined_epsilon, bounds=initial_guess,maxfev=100000)
-# Print the optimized parameters
-print("Optimized parameters for Sigma = 5 MPa:", params)
-
-
-### LMFIT MODEL ###
-model = Model(creep_model)
-parameters = model.make_params(C1=0.1,C2=20,C3=-20,C4=1000)
-parameters['C1'].set(min=0.00000000001,max=10)
-parameters['C2'].set(min=0.,max=50)
-parameters['C3'].set(min=-50,max=0)
-parameters['C4'].set(min=1,max=100000)
-result = model.fit(combined_epsilon,parameters,t=combined_time)
-
-# Print the optimized parameters
-print("Optimized parameters for Sigma = 5 MPa:", result.fit_report())
-
-params = []
-# Extract all parameters into a dictionary
-params_dict = {param_name: (param.value, param.stderr) for param_name, param in result.params.items()}
-
-# Extract the parameters
-for param_name, (value, stderr) in params_dict.items():
-    params.append(value)
-
-
-# Plot the experimental data and the fitted model
-
-def creep_model_test(t,stress,temp,C1,C2,C3,C4):
-    return ((1 / (stress**C2 * np.exp(-C4 / (273+temp)) * C1 * t * (1 - C3)))**(1 / (C3 - 1)))
-
-i=0
-
-for sigma in sigmaList:
-    timeSpace = np.logspace(np.log10(df[listEven[i]][0]), np.log10(df[listEven[i]][len(df[listEven[i]].dropna())-1]), num=100)
-    plt.plot(timeSpace,creep_model_test(timeSpace,sigma,combinedTemp_temp[i],*params),label=f'Fitted Model - {combinedTemp_temp[i]}°C - {sigma} MPa',linestyle='--',color=colors[i])
-    i=i+1
-        
-
-#Figure modification
-plt.xlabel('Time')
-# plt.xscale('log')
-plt.ylabel('Creep Strain')
-plt.title(f'Creep Strain Model Calibration for parameter: \n [C1 C2 C3 C4] = {params}')
-plt.legend()
-plt.grid(True)
-
-# Show the plot
-plt.savefig('fitted_data_40C lmfit', dpi=300)
-
-plt.show()
-
+if __name__ =='__main__':
+    main()
 
---
+++ mainProg.py
@@ -0,0 +1,134 @@
+# Import necessary libraries
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from creepFitting import creepFit
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import numpy as np
+
+# Function to handle file selection
+def select_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
+    entry_file.delete(0, tk.END)
+    entry_file.insert(0, file_path)
+
+# Function to convert plt fig to array
+def fig_to_array(fig):
+    # Draw the figure on the canvas
+    canvas = FigureCanvas(fig)
+    canvas.draw()
+    
+    # Get the RGBA buffer from the figure
+    buf = canvas.buffer_rgba()
+    
+    # Convert to a NumPy array
+    image = np.asarray(buf)
+    
+    return image
+
+# Function to handle processing
+def process_data():
+    # Read inputs
+    file_path = entry_file.get()
+    num_set1 = entry_num1.get()
+    num_set2 = entry_num2.get()
+    num_set3 = entry_num3.get()
+    option = var_option.get()
+    
+    # Read Excel file
+    df = pd.read_excel(file_path,header=None)
+    
+    # Process data (user-defined function)
+    results, imgPlt = creepFit(df, num_set1, num_set2, num_set3, option)
+    img = fig_to_array(imgPlt)
+
+    
+    # Display results and image
+    label_result1.config(text=f"Result 1: {results[0]}")
+    label_result2.config(text=f"Result 2: {results[1]}")
+    label_result3.config(text=f"Result 3: {results[2]}")
+    label_result4.config(text=f"Result 4: {results[3]}")
+    
+    # Display image
+    root.fig = plt.figure()
+    plt.imshow(img)
+    canvas = FigureCanvasTkAgg(root.fig, master=frame_image)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+# Function to save the displayed image
+def save_image():
+    file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+    root.fig.savefig(file_path)
+
+# Function to handle window close event
+def on_closing():
+    root.quit()
+    root.destroy()
+
+# Setup Tkinter window
+root = tk.Tk()
+root.title("Creep Calibration")
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+# Create UI elements
+label_file = tk.Label(root, text="Select Excel File:")
+entry_file = tk.Entry(root, width=50)
+button_file = tk.Button(root, text="Browse", command=select_file)
+
+label_num1 = tk.Label(root, text="Input Set 1:")
+entry_num1 = tk.Entry(root)
+
+label_num2 = tk.Label(root, text="Input Set 2:")
+entry_num2 = tk.Entry(root)
+
+label_num3 = tk.Label(root, text="Input Set 3:")
+entry_num3 = tk.Entry(root)
+
+var_option = tk.StringVar(value="default")
+label_option = tk.Label(root, text="Select Option:")
+radio_default = tk.Radiobutton(root, text="Default", variable=var_option, value="default")
+radio_lmfit = tk.Radiobutton(root, text="LMFit", variable=var_option, value="LMFIT")
+
+button_process = tk.Button(root, text="Process", command=process_data)
+
+label_result1 = tk.Label(root, text="Result 1: ")
+label_result2 = tk.Label(root, text="Result 2: ")
+label_result3 = tk.Label(root, text="Result 3: ")
+label_result4 = tk.Label(root, text="Result 4: ")
+
+frame_image = tk.Frame(root)
+button_save = tk.Button(root, text="Save Image", command=save_image)
+
+# Place UI elements on the window
+label_file.grid(row=0, column=0, padx=10, pady=5)
+entry_file.grid(row=0, column=1, padx=10, pady=5)
+button_file.grid(row=0, column=2, padx=10, pady=5)
+
+label_num1.grid(row=1, column=0, padx=10, pady=5)
+entry_num1.grid(row=1, column=1, padx=10, pady=5)
+
+label_num2.grid(row=2, column=0, padx=10, pady=5)
+entry_num2.grid(row=2, column=1, padx=10, pady=5)
+
+label_num3.grid(row=3, column=0, padx=10, pady=5)
+entry_num3.grid(row=3, column=1, padx=10, pady=5)
+
+label_option.grid(row=4, column=0, padx=10, pady=5)
+radio_default.grid(row=4, column=1, padx=10, pady=5)
+radio_lmfit.grid(row=4, column=2, padx=10, pady=5)
+
+button_process.grid(row=5, column=0, columnspan=3, padx=10, pady=10)
+
+label_result1.grid(row=6, column=0, columnspan=3, padx=10, pady=5)
+label_result2.grid(row=7, column=0, columnspan=3, padx=10, pady=5)
+label_result3.grid(row=8, column=0, columnspan=3, padx=10, pady=5)
+label_result4.grid(row=9, column=0, columnspan=3, padx=10, pady=5)
+
+frame_image.grid(row=0, column=3, rowspan=10, padx=10, pady=10)
+button_save.grid(row=10, column=0, columnspan=3, padx=10, pady=10)
+
+# Main loop
+root.mainloop()
